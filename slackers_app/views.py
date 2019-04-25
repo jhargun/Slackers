87bf42e4ab331abd26d984d276d2d3825e26071e
maximiliani2014 <maximiliani2014@gmail.com>
2019-04-25 13:26:18 -0400

    Added our first view

--- slackers_app/views.py
+++ slackers_app/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("It worked")
+    return render(request, 'slackers_templates/homepage.html', {"form":login_form})
 
 # Create your views here.
--- slackers_project/urls.py
+++ slackers_project/urls.py
@@ -19,5 +19,5 @@ from slackers_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index,)
+    path('', views.index, name="homepage")
 ]
