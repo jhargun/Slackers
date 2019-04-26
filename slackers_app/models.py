c44c2410c6c80dbf9e631883ef6e2bef56a26cec
Jaswin Hargun <jhargun20@andover.edu>
2019-04-26 11:51:03 -0400

    Made models and changed settings

--- slackers_app/models.py
+++ slackers_app/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+
+# If we're doing cookies, we're going to want a session id to be used to login, don't think we want it here, right?
+class User(models.Model):
+    # Do we want display name as well or should that just be username
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)  # Storing password as plaintext, terrible security, fix later
+
+    def __str__(self):  # You can return username and password as string, seems like bad security
+        return self.values()
+
+
+class Chat(models.Model):
+    id = models.CharField(max_length=100)
+    user1 = models.CharField(max_length=100)  # Use usernames here (or make unique id if usernames not unique)
+    user2 = models.CharField(max_length=100)
+    time = models.DateTimeField(auto_now=True)  # Changes to current time every time object changed
+
+    def __str__(self):
+        return self.values()
+
+
+class Message(models.Model):
+    chat = models.CharField(max_length=100)  # Use chat id
+    sender = models.CharField(max_length=100)  # Use sender's username
+    content = models.CharField(max_length=1000)  # Max length of messages is 1000 characters
+    time = models.DateTimeField(auto_now_add=True)  # Makes timestamp at time when object created
+
+    def __str__(self):
+        return self.values()
--- slackers_project/settings.py
+++ slackers_project/settings.py
@@ -75,12 +75,12 @@ WSGI_APPLICATION = 'slackers_project.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=False),
+}
+'''    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
+    }'''
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
