03e45f79134e815924c3331413f120dcba852095
maximiliani2014 <maximiliani2014@gmail.com>
2019-04-29 12:13:50 -0400

    Added real name field to User

--- slackers_app/forms.py
+++ slackers_app/forms.py
@@ -4,6 +4,7 @@ from django import forms
 class CreateForm(forms.Form):
     username = forms.CharField(label='Username', max_length=100)
     password = forms.CharField(label='Password', max_length=100)
+    real_name = forms.CharField(label='Real_Name', max_length=100)
 
 # form for logging in a user
 # Why do we have two of these? I feel like we could use the same form for both purposes
