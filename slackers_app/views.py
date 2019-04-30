142eebc503ec8acfa2ba976a92cf55acf7d5da70
jeremyzhou <jeremy.q.zhou@gmail.com>
2019-04-30 12:49:23 -0400

    added sessions to user_page

--- slackers_app/views.py
+++ slackers_app/views.py
@@ -12,10 +12,10 @@ def make(request):
         if form.is_valid():
             if User.objects.filter(username=form.cleaned_data['username']):
                 return render(request, 'slackers_app/ErrorPage.html',
-                    {
-                        'error_name': 'Username already taken',
-                        'index': reverse('slackers_app:make')
-                    })
+                            {
+                                'error_name': 'Username already taken',
+                                'index': reverse('slackers_app:make')
+                            })
             else:
                 u = User(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
                 u.save()
@@ -23,11 +23,11 @@ def make(request):
     else:
         form = CreateForm()
         return render(request, 'slackers_app/FormPage.html',
-            {
-                'form': form,
-                'page': reverse('slackers_app:make'),
-                'index': reverse('slackers_app:index')
-            })
+                    {
+                        'form': form,
+                        'page': reverse('slackers_app:make'),
+                        'index': reverse('slackers_app:index')
+                    })
 
 
 # Index page
@@ -36,22 +36,22 @@ def index(request):
         form = LoginForm(request.POST)
         if form.is_valid():
             if User.objects.filter(username=form.cleaned_data['username'], password=form.cleaned_data['password']):
-                request.session['username'] = form.cleaned_data['username']
+                request.session['username'] = form.cleaned_data['username']  # lol kind of insecure
                 return HttpResponseRedirect(reverse('slackers_app:home'))
             else:
                 return render(request, 'slackers_app/ErrorPage.html',
-                    {
-                        'error_name': 'Invalid username or password',
-                        'index': reverse('slackers_app:index')
-                    })
+                            {
+                                'error_name': 'Invalid username or password',
+                                'index': reverse('slackers_app:index')
+                            })
     else:
         form = LoginForm()
     return render(request, 'slackers_app/index.html',
-        {
-            'form': form,
-            'make': reverse('slackers_app:make'),
-            'index': reverse('slackers_app:index')
-        })
+                {
+                    'form': form,
+                    'make': reverse('slackers_app:make'),
+                    'index': reverse('slackers_app:index')
+                })
 
 
 '''
@@ -60,30 +60,38 @@ When we have cookies, add those here to figure out who the sender is.
 user_send is the person the message is sent to, NOT the sender. Sender determined with cookies.
 '''
 def user_page(request, user_send):
-    c = Chat.objects.filter(user1='''put user from cookie here''', user2=user_send)
+    u = request.session.get('username')
+    if not u:
+        return render(request, 'slackers_app/ErrorPage.html',
+                    {
+                        'error_name': 'User is not logged in',
+                        'index': reverse('slackers_app:index')
+                    })
+
+    c = Chat.objects.filter(user1=u, user2=user_send)
     # Checks if there is a chat with those 2 users; should we send error or just make a new one?
     if not c:
         return render(request, 'slackers_app/ErrorPage.html',
-                      {
+                    {
                           'error_name': 'No chat between these users exists',
                           'index': reverse('slackers_app:index')
-                      })
+                    })
 
     if request.method == 'POST':
         form = MessageForm(request.POST)
         if form.is_valid():
-            m = Message(chat=c.id, sender='''Cookie again''', content=form.cleaned_data['message'])
+            m = Message(chat=c.id, sender=u, content=form.cleaned_data['message'])
             m.save()
-            return HttpResponseRedirect(reverse('slackers_app:send', args=(user_send)))
+            return HttpResponseRedirect(reverse('slackers_app:send', args=(user_send,)))
 
     else:
-        form = LoginForm()
-    return render(request, 'slackers_app/FormPage.html',
-                  {
-                      'form': form,
-                      'page': reverse('slackers_app:send', args=(user_send)),
-                      'index': reverse('slackers_app:index'),
-                  })
+        form = MessageForm()
+    return render(request, 'slackers_app/home.html',
+                {
+                    'real_name': u.real_name,
+                    'form': form,
+                    'page': reverse('slackers_app:send', args=(user_send,))
+                })
 
 
 def home(request):
@@ -91,23 +99,23 @@ def home(request):
         form = MessageForm(request.POST)
         if form.is_valid():
             return render(request, 'slackers_app/ErrorPage.html',
-                {
-                    'error_name': 'Message functionality has not been implemented yet sad',
-                    'index': reverse('slackers_app:home')
-                })
+                        {
+                            'error_name': 'Message functionality has not been implemented yet sad',
+                            'index': reverse('slackers_app:home')
+                        })
     else:
         if request.session.get('username'):
             form = MessageForm()
             u = User.objects.get(username=request.session.get('username'))
             return render(request, 'slackers_app/home.html',
-            {
-                'real_name': u.real_name,
-                'form': form,
-                'page': reverse('slackers_app:home')
-            })
+                        {
+                            'real_name': u.real_name,
+                            'form': form,
+                            'page': reverse('slackers_app:home')
+                        })
         else:
             return render(request, 'slackers_app/ErrorPage.html',
-                {
-                    'error_name': 'User is not logged in',
-                    'index': reverse('slackers_app:index')
-                })
+                        {
+                            'error_name': 'User is not logged in',
+                            'index': reverse('slackers_app:index')
+                        })
