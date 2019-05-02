c7fe63253e8bc8738c2124e52f8aadc92ac497cd
Jaswin Hargun <jhargun20@andover.edu>
2019-05-02 12:10:02 -0400

    Added Comma

--- slackers_app/urls.py
+++ slackers_app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
 
-app_name='slackers_app'
+app_name = 'slackers_app'
 urlpatterns = [
     path('', views.index, name="index"),
     path('make', views.make, name="make"),
     path('home', views.home, name='home'),
     path('edit', views.edit, name='edit'),
-    path('change_chat/<str:chat_id>', views.switch_chat, name='c_edit')
-    path('chat_make', views.make_chat, name='c_make')
+    path('change_chat/<str:chat_id>', views.switch_chat, name='c_edit'),
+    path('chat_make', views.make_chat, name='c_make'),
 ]
